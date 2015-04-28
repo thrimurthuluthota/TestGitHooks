3ece5f354e9cd7d75e739c4ba7745a336d98eca8
unknown <1500109@INNR90A3XRR.zone1.scb.net>
2015-04-28 09:56:20 +0530

    [ok] do it for commit- msg

--- commit-msg.js
+++ commit-msg.js
@@ -2,9 +2,12 @@ var fs = require('fs');
 require('shelljs/global');
 
 var fileName = process.argv[2];
+console.log('fileName ' + fileName);
 var message = fs.readFileSync(fileName, "utf8").trim();
+console.log('message ' + message);
 
 var regEx = new RegExp(/^\[.+\]/);
+console.log('regEx ' + regEx);
 
 if(!regEx.test(message)) {
   echo("  Error: Commit message not in proper format")
